68aff12672169bc5d2495efd5b6a5890ab76204b
안혜연 <ahy020606@naver.com>
2026-03-23 14:25:19 +0900

    fix: DisclosureRepository 수정

--- src/main/java/com/briefin/domain/disclosures/repository/DisclosuresRepository.java
+++ src/main/java/com/briefin/domain/disclosures/repository/DisclosuresRepository.java
@@ -2,6 +2,7 @@ package com.briefin.domain.disclosures.repository;
 
 import com.briefin.domain.disclosures.entity.Disclosures;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 

ade60b397ace6e2f37558ef5c45b29ef98755bcc
heessunny <kimheesun107@gmail.com>
2026-03-31 13:17:40 +0900

    fix: 검색 리스트 웹소켓 연결

--- src/main/java/com/briefin/domain/companies/service/CompanyUpdateService.java
+++ src/main/java/com/briefin/domain/companies/service/CompanyUpdateService.java
@@ -7,4 +7,9 @@ public interface CompanyUpdateService {
      * @param marketCap 시가총액 값
      */
     void updatePrice(String ticker, Long marketCap);
+
+    /**
+     * 개별 종목의 현재가, 등락률, 시가총액을 독립된 트랜잭션으로 업데이트합니다.
+     */
+    void updatePriceAndChangeRate(String ticker, double currentPrice, double changeRate, long marketCap);
 }
